f8e6ae2e31fffa1fb00e4bef4f9a2cf11f72563b
2611saurabh <saurabh.kr026@gmail.com>
2023-07-29 20:07:16 +0530

    Upgrading

--- src/Car.java
+++ src/Car.java
@@ -1,17 +1,36 @@
 
 public class Car {
-    private String make;
+    private String make = "Tesla";
 
-    private String model;
+    private String model = "Model X";
 
-    private String color;
+    private String color = "Gray";
 
-    private int doors;
+    private int doors = 2;
 
-    private boolean convertible;
+    private boolean convertible = true;
+
+
+
+    //here we initilize getter to access private modifier
+    public String getMake(){
+        return make;
+    }
+    public String getModel(){
+        return model;
+    }
+    public String getColor(){
+        return color;
+    }
+    public int getDoor(){
+        return doors;
+    }
+    public boolean isConvertible(){
+        return convertible;
+    }
 
     public void describeCar(){
-        System.out.println(doors + "-Door"+
+        System.out.println(doors + " -Door"+
                 color + " " +
                 make + " " +
                 model + " " +
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Car car = new Car();
+//        car.make = "Swfit";
+//        car.model = "MAruti";
+//        car.color = "Red";
+        System.out.println("make = "+car.getMake());
+        System.out.println("model = "+car.getModel());
         car.describeCar();
 
     }
